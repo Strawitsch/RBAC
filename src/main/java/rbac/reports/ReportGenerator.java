1f372909713a551a8d4f6effbe5dbef92f27a2f9
Dmitry Ivanenko <dimaivanenko11@gmail.com>
2026-03-22 21:23:32 +0700

    Subtask 4.3. Add ReportGenerator

---
+++ src/main/java/rbac/reports/ReportGenerator.java
@@ -0,0 +1,26 @@
+package rbac.reports;
+
+import rbac.core.*;
+import rbac.managers.*;
+import rbac.utils.FormatUtils;
+
+import java.io.*;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class ReportGenerator {
+
+    }
+
+    public static String generateRoleReport(RoleManager roleManager, AssignmentManager assignmentManager) {
+
+    }
+
+    public static String generatePermissionMatrix(UserManager userManager, AssignmentManager assignmentManager) {
+
+    }
+
+    public static void exportToFile(String report, String filename) throws IOException {
+
+    }
+}
